01f81ed4148c575fa002e245162442fed8cbc45f
Sammwy <sammwy.dev@gmail.com>
2023-12-30 23:53:57 -0300

    fix: endpoint

--- contexts/projects/projects-provider.tsx
+++ contexts/projects/projects-provider.tsx
@@ -4,9 +4,7 @@ import Project from '@/lib/project';
 
 import { ProjectsContext } from './projects-context';
 
-const DEV_ENDPOINT = '/api/dev_projects';
-const PROD_ENDPOINT =
-  'https://github.com/staroverlay/frontend/raw/main/data/projects.json';
+const ENDPOINT = '/projects.json';
 
 export function ProjectsProvider({ children }: PropsWithChildren) {
   const [fetching, setFetching] = useState(false);
@@ -14,8 +12,7 @@ export function ProjectsProvider({ children }: PropsWithChildren) {
 
   const fetchProjects = () => {
     const isDEV = process.env.NODE_ENV == 'development';
-    const endpoint = isDEV ? DEV_ENDPOINT : PROD_ENDPOINT;
-    fetch(endpoint)
+    fetch(ENDPOINT)
       .then((req) => req.json())
       .then((data) => setProjects(data.projects));
   };
